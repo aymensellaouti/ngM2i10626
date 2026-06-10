c78e1c131e4627675172c4c418e7bd0309b265cb
aymensellaouti <aymen.sellaouti@gmail.com>
2026-06-10 16:38:24 +0100

    exo miniWord

--- src/app/components/mini-word/mini-word.component.ts
+++ src/app/components/mini-word/mini-word.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mini-word',
   templateUrl: './mini-word.component.html',
-  styleUrls: ['./mini-word.component.css']
+  styleUrls: ['./mini-word.component.css'],
+  imports: [FormsModule]
 })
 export class MiniWordComponent {
-
+  // State
+  color = signal('red');
+  fontSize = signal(70);
+  fontFamily = signal('garamond')
 }
