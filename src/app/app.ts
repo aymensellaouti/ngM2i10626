3357d177c17d7a537c2f6288f28a828154b4b0cb
aymensellaouti <aymen.sellaouti@gmail.com>
2026-06-10 09:22:48 +0100

    Add bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
+  // Si tu veux appeler ce composant crée une balsie <app-root/>
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // importe tous ce qui est nécessaire pour le HTML
+  imports: [],
+  // c'est le html que je gére (moi le composant)
   templateUrl: './app.html',
+  // Le style associé uniquement à ce composant
   styleUrl: './app.css'
 })
 export class App {
+  // State : Son état représenté par les attributs
   protected readonly title = signal('ngM2i10626');
+
+  // Behaviour: Comportement, représenté par les méthodes
 }
