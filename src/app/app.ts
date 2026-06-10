a1e09bc1b2af5832ce55997727c7c6db6dc540e8
aymensellaouti <aymen.sellaouti@gmail.com>
2026-06-10 11:54:14 +0100

    mise en place de rotating card

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,14 @@ import { Component, signal } from '@angular/core';
 import { First } from "./components/first/first";
 import { Second } from "./components/second/second";
 import { Color } from "./components/color/color";
+import { TwoWay } from "./components/two-way/two-way";
+import { RotatingCard } from "./components/rotating-card/rotating-card";
 
 @Component({
   // Si tu veux appeler ce composant crée une balsie <app-root/>
   selector: 'app-root',
   // importe tous ce qui est nécessaire pour le HTML
-  imports: [First, Second, Color],
+  imports: [RotatingCard],
   // c'est le html que je gére (moi le composant)
   templateUrl: './app.html',
   // Le style associé uniquement à ce composant
