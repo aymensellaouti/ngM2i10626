b786b1f023ce71cb3974565674af6676758718d7
aymensellaouti <aymen.sellaouti@gmail.com>
2026-06-11 15:14:24 +0100

    mise en place de login form et exemple template driven form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { Color } from './components/color/color';
 import { Second } from './components/second/second';
 import { DetailsCvComponent } from './cv/details-cv/details-cv.component';
 import { NF404 } from './composants/nf404/nf404';
+import { Login } from './auth/login/login';
 
 export const routes: Routes = [
   // Ici qu'on va définir nos routes
@@ -15,6 +16,7 @@ export const routes: Routes = [
   {path: 'cv', component: CvPage},
   {path: 'cv/:id', component: DetailsCvComponent},
   {path: 'word', component: MiniWordComponent},
+  {path: 'login', component: Login},
   {path: 'color', component: Color},
   {path: 'hello/:name/:firstname', component: Second},
   {path: '**', component: NF404},
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 import { Navbar } from "./composants/navbar/navbar";
+import { TestForm } from "./templateDriveform/test-form/test-form";
 
 @Component({
   // Si tu veux appeler ce composant crée une balsie <app-root/>
   selector: 'app-root',
   // importe tous ce qui est nécessaire pour le HTML
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, TestForm],
   // c'est le html que je gére (moi le composant)
   templateUrl: './app.html',
   // Le style associé uniquement à ce composant
