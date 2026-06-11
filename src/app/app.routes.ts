056f0a3bae8b6ad468b74993d947986d58cdf69f
aymensellaouti <aymen.sellaouti@gmail.com>
2026-06-11 13:35:11 +0100

    mise en place du routeur et du menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { First } from './components/first/first';
+import { TodoComponent } from './todo/todo/todo.component';
+import { CvPage } from './cv/cv-page/cv-page';
+import { MiniWordComponent } from './components/mini-word/mini-word.component';
+import { Color } from './components/color/color';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Ici qu'on va définir nos routes
+  {path: '', component: First},
+  {path: 'todo', component: TodoComponent},
+  {path: 'cv', component: CvPage},
+  {path: 'word', component: MiniWordComponent},
+  {path: 'color', component: Color},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-import { TtcComponent } from "./signals/ttc/ttc.component";
-import { Pere } from "./commInterCompo/pere/pere";
-import { CvPage } from "./cv/cv-page/cv-page";
-import { NgStyleComponent } from "./directives/ng-style/ng-style";
-import { MiniWordComponent } from "./components/mini-word/mini-word.component";
-import { NgclassComponent } from "./directives/ngclass/ngclass";
-import { TodoComponent } from "./todo/todo/todo.component";
+import { RouterOutlet } from "@angular/router";
+import { Navbar } from "./composants/navbar/navbar";
 
 @Component({
   // Si tu veux appeler ce composant crée une balsie <app-root/>
   selector: 'app-root',
   // importe tous ce qui est nécessaire pour le HTML
-  imports: [TtcComponent, Pere, CvPage, NgStyleComponent, MiniWordComponent, NgclassComponent, TodoComponent],
+  imports: [RouterOutlet, Navbar],
   // c'est le html que je gére (moi le composant)
   templateUrl: './app.html',
   // Le style associé uniquement à ce composant
