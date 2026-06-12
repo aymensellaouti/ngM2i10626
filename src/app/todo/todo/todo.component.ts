bfa90527c6dcb85a7a618ec8785f65e185ceb378
aymensellaouti <aymen.sellaouti@gmail.com>
2026-06-12 13:54:52 +0100

    exemple httpResource

--- src/app/todo/todo/todo.component.ts
+++ src/app/todo/todo/todo.component.ts
@@ -3,26 +3,30 @@ import { Todo } from "../model/todo";
 import { FormsModule } from "@angular/forms";
 import { TodoService } from "../service/todo.service";
 import { TodoApi } from "../model/todo-api.model";
+import { httpResource } from "@angular/common/http";
+import { APP_API } from "../../config/app-api.config";
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css'],
-  imports: [FormsModule]
+  imports: [FormsModule],
 })
 export class TodoComponent {
   todoService = inject(TodoService);
   todos = this.todoService.getTodos();
-  todosApi =signal<TodoApi[]>([]);
-  todo = signal<Todo>(new Todo())
+  todosApi = signal<TodoApi[]>([]);
+  todo = signal<Todo>(new Todo());
+  todosResources = httpResource<TodoApi[]>(() => `${APP_API.todos}`,{defaultValue: [],},
+  );
   constructor() {
-    this.todoService.getTodosFromApi().subscribe({
-      next: (todosFromApi) => {
-        this.todosApi.set(todosFromApi)
-      },
-      error: (e) => {},
-      complete: () => {}
-    })
+    // this.todoService.getTodosFromApi().subscribe({
+    //   next: (todosFromApi) => {
+    //     this.todosApi.set(todosFromApi);
+    //   },
+    //   error: (e) => {},
+    //   complete: () => {},
+    // });
   }
 
   addTodo() {
