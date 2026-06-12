5ac28ebef108a7529c895115583a01f6cf681455
aymensellaouti <aymen.sellaouti@gmail.com>
2026-06-12 10:37:43 +0100

    authGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,13 +9,14 @@ import { DetailsCvComponent } from './cv/details-cv/details-cv.component';
 import { NF404 } from './composants/nf404/nf404';
 import { Login } from './auth/login/login';
 import { AddCvComponent } from './cv/add-cv/add-cv.component';
+import { authGuard } from './auth/guard/auth-guard';
 
 export const routes: Routes = [
   // Ici qu'on va définir nos routes
   {path: '', component: First},
   {path: 'todo', component: TodoComponent},
   {path: 'cv', component: CvPage},
-  {path: 'cv/add', component: AddCvComponent, canActivate: []},
+  {path: 'cv/add', component: AddCvComponent, canActivate: [authGuard]},
   {path: 'cv/:id', component: DetailsCvComponent},
   {path: 'word', component: MiniWordComponent},
   {path: 'login', component: Login},
--- src/app/auth/guard/auth-guard.ts
+++ src/app/auth/guard/auth-guard.ts
@@ -1,9 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+import { APP_ROUTES } from '../../config/app-routes.config';
 
 export const authGuard: CanActivateFn = (route, state) => {
   // Il faudra vérifier est ce que la personne a le droit ou non
   // d'accéder à la route
-
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  if (!authService.isAuthenticated()){
+    router.navigate([APP_ROUTES.login]);
+    return false;
+  }
   // On retourne true pour dire tu passes; false sinon
   return true;
 };
