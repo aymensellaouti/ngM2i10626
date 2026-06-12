aa72a50e468be786575951be15181f796389cc06
aymensellaouti <aymen.sellaouti@gmail.com>
2026-06-12 13:40:13 +0100

    with toSignal

--- src/app/cv/cv-page/cv-page.ts
+++ src/app/cv/cv-page/cv-page.ts
@@ -1,12 +1,14 @@
 import { Component, inject, input, signal } from '@angular/core';
 import { Cv } from '../model/cv.model';
-import { CvsList } from "../cvs-list/cvs-list";
-import { CvCard } from "../cv-card/cv-card";
+import { CvsList } from '../cvs-list/cvs-list';
+import { CvCard } from '../cv-card/cv-card';
 import { DatePipe, UpperCasePipe } from '@angular/common';
 import { LoggerService } from '../../services/logger-service';
 import { ToastrService } from 'ngx-toastr';
 import { CvService } from '../services/cv.service';
-import { EmbaucheComponent } from "../embauche/embauche.component";
+import { EmbaucheComponent } from '../embauche/embauche.component';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { catchError, of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-cv-page',
@@ -17,23 +19,37 @@ import { EmbaucheComponent } from "../embauche/embauche.component";
 export class CvPage {
   cvService = inject(CvService);
   today = signal(new Date());
-  cvs = signal<Cv[]>([]);
+  isLoading = signal(true);
+  erreur =signal('');
+  cvs = toSignal(
+    this.cvService.getCvsFromApi().pipe(
+      tap(response=> {this.isLoading.set(false)}),
+      catchError((e) => {
+        this.erreur.set('y a une erreur')
+        const fakeCvs = this.cvService.getCvs();
+        return of(fakeCvs());
+      }),
+    ),
+    {
+      initialValue: [],
+    },
+  );
   logger = inject(LoggerService);
   selectedCv = this.cvService.selectedCv;
   toastr = inject(ToastrService);
 
   constructor() {
-    this.toastr.info('Bienvenu dans notre CvTech :D')
+    this.toastr.info('Bienvenu dans notre CvTech :D');
     this.logger.log('cc je suis le cvComponent');
-    this.cvService.getCvsFromApi().subscribe({
-      next: (cvs) => {
-        this.cvs.set(cvs);
-      },
-      error: (e) => {
-        const fakeCvs = this.cvService.getCvs();
-        this.cvs.set(fakeCvs());
-        this.toastr.error("Attention les datas sont fictives veuillez contacter l'admin !!");
-      },
-    })
+    // this.cvService.getCvsFromApi().subscribe({
+    //   next: (cvs) => {
+    //     this.cvs.set(cvs);
+    //   },
+    //   error: (e) => {
+    //     const fakeCvs = this.cvService.getCvs();
+    //     this.cvs.set(fakeCvs());
+    //     this.toastr.error("Attention les datas sont fictives veuillez contacter l'admin !!");
+    //   },
+    // })
   }
 }
--- src/app/cv/details-cv/details-cv.component.ts
+++ src/app/cv/details-cv/details-cv.component.ts
@@ -1,46 +1,56 @@
-import { Component, inject, signal } from "@angular/core";
-import { Cv } from "../model/cv.model";
-import { CvService } from "../services/cv.service";
-import { ActivatedRoute, Router } from "@angular/router";
-import { APP_ROUTES } from "../../config/app-routes.config";
-import { DefaultImagePipe } from "../pipes/default-image-pipe";
+import { Component, inject, signal } from '@angular/core';
+import { Cv } from '../model/cv.model';
+import { CvService } from '../services/cv.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { APP_ROUTES } from '../../config/app-routes.config';
+import { DefaultImagePipe } from '../pipes/default-image-pipe';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { catchError, EMPTY } from 'rxjs';
 
 @Component({
   selector: 'app-details-cv',
   templateUrl: './details-cv.component.html',
   styleUrls: ['./details-cv.component.css'],
-  imports: [DefaultImagePipe]
+  imports: [DefaultImagePipe],
 })
 export class DetailsCvComponent {
-  cv = signal<Cv | null>(null);
   cvService = inject(CvService);
   acr = inject(ActivatedRoute);
+  cv = toSignal(
+    this.cvService.getCvByIdFromApi(this.acr.snapshot.params['id']).pipe(
+      catchError((e) => {
+        this.router.navigate([APP_ROUTES.cv]);
+        return EMPTY;
+      }),
+    ),
+    { initialValue: null },
+  );
   router = inject(Router);
   constructor() {
-    const id = this.acr.snapshot.params['id'];
-    //this.cv.set(this.cvService.findCvById(id));
-    this.cvService.getCvByIdFromApi(id).subscribe({
-      next: (cv) => {
-        this.cv.set(cv);
-      },
-      error: (e) => {
-        this.router.navigate([APP_ROUTES.cv]);
-      }
-    })
+    // const id = this.acr.snapshot.params['id'];
+    // //this.cv.set(this.cvService.findCvById(id));
+    // this.cvService.getCvByIdFromApi(id).subscribe({
+    //   next: (cv) => {
+    //     this.cv.set(cv);
+    //   },
+    //   error: (e) => {
+    //     this.router.navigate([APP_ROUTES.cv]);
+    //   },
+    // });
     // if (!this.cv())
   }
 
   delete() {
     const cv = this.cv();
     if (cv) {
-       this.cvService.deleteCvByIdFromApi(cv.id).subscribe({
-         next: (cv) => {
-           this.router.navigate([APP_ROUTES.cv]);
-         },
-         error: (e) => {
+      this.cvService.deleteCvByIdFromApi(cv.id).subscribe({
+        next: (cv) => {
+          this.router.navigate([APP_ROUTES.cv]);
+        },
+        error: (e) => {
           console.log(e);
-         },
-       });
+        },
+      });
     }
   }
 }
